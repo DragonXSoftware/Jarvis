35c95ef0a5c6a53849010bb38840cf328573155f
Austin Schwebel <abschw0421@gmail.com>
2025-03-23 10:43:27 -0400

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "------------------------------------------------"
+echo "  Jarvis UI Launcher"
+echo "  Make sure Ollama is running before continuing."
+echo "------------------------------------------------"
+
+# Pause for a moment
+sleep 2
+
+# Run the Jarvis UI
+python3 JarvisUI.py
